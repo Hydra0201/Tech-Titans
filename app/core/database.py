4ad754520cc2c758531ab768f796c88ea8e2933c
Mann <mann55kumar@gmail.com>
2025-05-23 13:46:14 +1000

    Created PostgreSQL schema with SQLAlchemy models

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Update this with your actual credentials
 DATABASE_URL = "postgresql://postgres:6266@localhost:5432/carbonbalance"
