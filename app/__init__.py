ccef6110dcc300e8441c39bcfb2a92a735666640
Roghan Behm <roghanbehm@Mac.modem>
2025-07-28 20:36:45 +1000

    Add flask example method call route

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,10 @@ def create_app():
     app = Flask(__name__)
 
     from app.routes.example_route import example_bp
+    from app.routes.get_metrics import get_metrics_bp
+
     app.register_blueprint(example_bp, url_prefix='/api')
+    app.register_blueprint(get_metrics_bp, url_prefix='/api')
 
     return app
 
